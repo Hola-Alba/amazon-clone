776c752f8a89b6a531f884bd3e7ec60b12af44f5
alba <alba@impoexpo.info>
2023-01-26 19:13:46 -0500

    ADD contract and test files

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.9;
 
 contract Dappazon {
+    //Code goes here...
     address public owner;
 
     struct Item {
@@ -14,80 +15,91 @@ contract Dappazon {
         uint256 stock;
     }
 
-    struct Order {
+    struct Order{
         uint256 time;
         Item item;
     }
 
     mapping(uint256 => Item) public items;
-    mapping(address => mapping(uint256 => Order)) public orders;
     mapping(address => uint256) public orderCount;
+    mapping(address => mapping(uint256 => Order)) public orders;
 
     event Buy(address buyer, uint256 orderId, uint256 itemId);
     event List(string name, uint256 cost, uint256 quantity);
 
-    modifier onlyOwner() {
+    modifier onlyOwner(){
         require(msg.sender == owner);
         _;
     }
-
-    constructor() {
+    
+    constructor(){
         owner = msg.sender;
     }
 
+    //List products
     function list(
-        uint256 _id,
-        string memory _name,
-        string memory _category,
-        string memory _image,
-        uint256 _cost,
-        uint256 _rating,
-        uint256 _stock
+    uint256 _id, 
+    string memory _name,
+    string memory _category,
+    string memory _image,
+    uint256 _cost,
+    uint256 _rating,
+    uint256 _stock
     ) public onlyOwner {
-        // Create Item
+        //Code goes here...
+       
+        //Create Item struct
         Item memory item = Item(
             _id,
-            _name,
-            _category,
-            _image,
-            _cost,
-            _rating,
+            _name, 
+            _category, 
+            _image, 
+            _cost, 
+            _rating, 
             _stock
         );
 
-        // Add Item to mapping
+        //Save Item struct to blockchain
         items[_id] = item;
 
-        // Emit event
+        //Emit an event
         emit List(_name, _cost, _stock);
     }
 
+
+    //Buy products
     function buy(uint256 _id) public payable {
-        // Fetch item
+        //Fetch item
         Item memory item = items[_id];
 
-        // Require enough ether to buy item
+        //Require enough ether to buy item
         require(msg.value >= item.cost);
 
-        // Require item is in stock
-        require(item.stock > 0);
-
-        // Create order
+        //Require item is in stock
+        require(item.stock >0);
+       
+        //Create an order
         Order memory order = Order(block.timestamp, item);
 
-        // Add order for user
-        orderCount[msg.sender]++; // <-- Order ID
+        //Save order to chain
+        orderCount[msg.sender] ++; // Order ID
         orders[msg.sender][orderCount[msg.sender]] = order;
 
-        // Subtract stock
+        //Substrack stock
         items[_id].stock = item.stock - 1;
 
-        // Emit event
+        //Emit event
         emit Buy(msg.sender, orderCount[msg.sender], item.id);
     }
 
-    function withdraw() public onlyOwner {
+    //Withdraw funds
+    function withdraw() public onlyOwner{
         (bool success, ) = owner.call{value: address(this).balance}("");
         require(success);
     }
+
 }
+
+
+
+
